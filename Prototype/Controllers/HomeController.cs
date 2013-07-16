8ad6c40dd637ee30217be8e2f1c27ce0df662813
Shaun Luttin <shaunluttin@gmail.com>
2013-07-16 15:28:45 -0700

    Add the domain model.

---
+++ Prototype/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Prototype.Models;
+
+namespace Prototype.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            using (var db = new OrderSystemContext())
+            { 
+                //creat objects
+                Vendor vendor = new Vendor();
+                vendor.VendorName = "Test Vendor";
+
+                Product product = new Product();
+                product.Vendor = vendor;
+                product.ProductDescription = "Test product";
+
+                //add objects to context
+                db.Vendors.Add(vendor);
+                db.Products.Add(product);
+
+                //save context
+                db.SaveChanges();
+
+                //make a query
+                var query = from v in db.Vendors
+                            orderby v.VendorName
+                            select v;
+
+                //execute the query
+                foreach (var v in query)
+                {
+                    var name = v.VendorName;
+                }
+
+                //done
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Prototype/Models/DomainModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Prototype.Models
+{
+    public class Vendor
+    {
+        public int VendorID { get; set; }
+        public string VendorName { get; set; }
+        public virtual List<Product> Products { get; set; }
+    }
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductDescription { get; set; }
+        public int VendorID { get; set; }
+        public virtual Vendor Vendor { get; set; }
+    }
+    public class OrderSystemContext : DbContext
+    {
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
